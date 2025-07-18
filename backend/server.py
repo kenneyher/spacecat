3ebaf50e6318f787f057301960bb47e49152fca9
supercancala <supercancala@gmail.com>
2025-07-17 22:05:06 -0600

    Add code for echo server

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,23 @@
 import socket
 
-IP_ADDR='127.0.0.1'
+HOST='127.0.0.1'
 PORT=12345
 
-sock = socket.create_server((IP_ADDR, PORT))
+sock = socket.socket(
+    family = socket.AF_INET,    #AF_INET = IPv4
+    type = socket.SOCK_STREAM
+)
 
-print(sock)
+sock.bind((HOST, PORT))
+print(f"Socket bound to {HOST}:{PORT}")
+sock.listen()
+conn, addr = sock.accept()
+
+with conn : 
+    print(f"connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+            

d8bd88c44ce77adb7acace624500dfcb26537d29
Kenneth <kencris007@gmail.com>
2025-07-18 16:21:54 -0600

    Add instructions and actions to client side; active connection with server

--- client/client.py
+++ client/client.py
@@ -1,15 +1,47 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 12345
+HOST:str = "127.0.0.1"
+PORT:int = 12345
+ACTIONS:tuple[str] = ('/send', '/exit')
 
-sock = socket.socket(
+sock:socket.socket = socket.socket(
     family = socket.AF_INET,
     type = socket.SOCK_STREAM
 )
 
 sock.connect((HOST, PORT))
-msg = input("Enter message to server >> ")
-sock.sendall(msg.encode())
-data = sock.recv(1024)
-print(f"received: {data.decode()}")
+instruction = ""
+exiting = False
+
+def help() -> None:
+    print('/send -> send message to server')
+    print('/end -> end connection with server')
+    print('/help -> print available options')
+
+print(f'Connected to {HOST}:{PORT}')
+print('Please select the action to perform ---------------')
+help()
+
+while not exiting:
+    instruction:str = input('>>> ').lower()
+    if instruction not in ACTIONS:
+        print('Please provide a correct option')
+    elif instruction == '/send':
+        msg = input('> Message content >>>')
+        sock.sendall(msg.encode())
+
+        print('<', sock.recv(1024).decode())
+    elif instruction == '/end':
+        option = ''
+        while option not in ('y', 'n', 'yes', 'no'):
+            option = input('Exit (Y/N) ?>> ').lower()
+            if option in ('y', 'yes'):
+                exiting = True
+                break
+            elif option in ('n', 'no'):
+                break
+    elif instruction == '/help':
+        help()
+
+sock.close()
+

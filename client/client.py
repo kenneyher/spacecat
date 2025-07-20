8b9110bc7ba93f9d7237c15a8c56eca13f63d7c1
supercancala <supercancala@gmail.com>
2025-07-19 21:26:52 -0600

    Use dict for command handling
     - Create functions for commands
     - Link functions to dictionary
     - Refactor main loop

--- client/client.py
+++ client/client.py
@@ -2,7 +2,26 @@ import socket
 
 HOST:str = "127.0.0.1"
 PORT:int = 12345
-ACTIONS:tuple[str] = ('/send', '/exit')
+
+def send_msg(sock:socket.socket, msg:str) -> None:
+    sock.sendall(msg.encode())
+
+
+def help() -> None:
+    print('/send -> send message to server')
+    print('/end -> end connection with server')
+    print('/help -> print available options')
+
+
+def end(sock:socket.socket) -> None:
+    sock.close()
+
+
+ACTIONS:dict = {
+    "/send" : send_msg,
+    "/end" : end,
+    "/help" : help
+}
 
 sock:socket.socket = socket.socket(
     family = socket.AF_INET,
@@ -13,35 +32,25 @@ sock.connect((HOST, PORT))
 instruction = ""
 exiting = False
 
-def help() -> None:
-    print('/send -> send message to server')
-    print('/end -> end connection with server')
-    print('/help -> print available options')
-
 print(f'Connected to {HOST}:{PORT}')
 print('Please select the action to perform ---------------')
 help()
 
 while not exiting:
-    instruction:str = input('>>> ').lower()
-    if instruction not in ACTIONS:
+    # Input will be parsed with split(), Minecraft style
+    command:list[str] = input('>>> ').lower().split(" ")
+    if command[0] not in ACTIONS:
         print('Please provide a correct option')
-    elif instruction == '/send':
-        msg = input('> Message content >>>')
-        sock.sendall(msg.encode())
-
-        print('<', sock.recv(1024).decode())
-    elif instruction == '/end':
-        option = ''
-        while option not in ('y', 'n', 'yes', 'no'):
-            option = input('Exit (Y/N) ?>> ').lower()
-            if option in ('y', 'yes'):
-                exiting = True
-                break
-            elif option in ('n', 'no'):
-                break
-    elif instruction == '/help':
-        help()
+    else:
+        if command[0] == "/send":
+            ACTIONS[command[0]](sock, " ".join(command[1:]))
+            msg = sock.recv(1024)
+            print(msg.decode())
+        elif command[0] == "/end":
+            exiting = True
+            ACTIONS[command[0]](sock)
+        else:
+            ACTIONS[command[0]]()
 
 sock.close()
 
